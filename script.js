4e35034e9a4540150a48daba32d930bf07f83a5b
Malgorzata Berdys <berdysm@gmail.com>
2021-11-12 18:40:49 +0100

    Add ball, paddle and interactions

--- script.js
+++ script.js
@@ -1,2 +1,83 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
+let x = canvas.width/2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+let ballRadius = 10;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth)/2;
+let rightPressed = false;
+let leftPressed = false;
+
+
+function keyDownHandler(e) {
+    //39 - right arrow button, 37 - left
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    } else if (e.keyCode ==37) {
+        leftPressed = true;
+    };
+};
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    } else if (e.keyCode ==37) {
+        leftPressed = false;
+    };
+};
+
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+};
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+};
+
+function draw() {
+    //clearRect - nadaje granice
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    } else if(y + dy > canvas.height - ballRadius) {
+        //jeśli jest pomiędzy lewym a prawym przegiem paddle
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            alert('GAME OVER');
+            document.location.reload();
+        };
+    };
+
+    if(x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+        dx = -dx;
+    };
+
+    if(rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    } else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    };
+
+    x += dx;
+    y += dy;
+};
+
+setInterval(draw, 10);
